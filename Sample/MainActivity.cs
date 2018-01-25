ee97ddb24614c63fb349ecaebe70c6ac0ea2c68a
Artyom Voronov <auvoronov@gmail.com>
2018-01-25 11:13:58 +0300

    Initial commit

---
+++ Sample/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.OS;
+using IM.Crisp.Sdk;
+
+namespace Sample
+{
+    [Activity(Label = "Sample", MainLauncher = true, Icon = "@mipmap/icon")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            Crisp.Initialize(this);
+            Crisp.Instance.WebsiteId = "Your_Website_Id";
+            Crisp.User.SetEmail("user@email.com");
+            Crisp.User.SetNickname("NickName");
+
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
+
